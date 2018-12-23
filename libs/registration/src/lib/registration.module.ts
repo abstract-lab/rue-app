a4e9a5a94fc74ad26713dae42ac1c848e8b03b4a
Mihai Osvat <mihai.osvat@nordlogic.com>
2018-12-23 10:43:12 +0200

    Started registration component

--- libs/registration/src/lib/registration.module.ts
+++ libs/registration/src/lib/registration.module.ts
@@ -5,18 +5,20 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { REGISTRATION_FEATURE_KEY, initialState as registrationInitialState, registrationReducer } from './+state/registration.reducer';
 import { RegistrationEffects } from './+state/registration.effects';
+import { RegistrationComponent } from './containers/registration/registration.component';
 
 @NgModule({
   imports: [
     CommonModule,
 
     RouterModule.forChild([
-      /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
+      {path: '', pathMatch: 'full', component: RegistrationComponent}
     ]),
 
     StoreModule.forFeature(REGISTRATION_FEATURE_KEY, registrationReducer, { initialState: registrationInitialState }),
 
     EffectsModule.forFeature([RegistrationEffects])
-  ]
+  ],
+  declarations: [RegistrationComponent]
 })
 export class RegistrationModule {}
