ae5742e91b9e2f2e647464c936909495b27bab83
Mihai Osvat <mihai.osvat@nordlogic.com>
2018-12-28 08:59:30 +0200

    Fixing tests

--- libs/registration/src/lib/+state/registration.selectors.spec.ts
+++ libs/registration/src/lib/+state/registration.selectors.spec.ts
@@ -3,49 +3,34 @@ import { registrationQuery } from './registration.selectors';
 
 describe('Registration Selectors', () => {
   const ERROR_MSG = 'No Error Available';
-  const getRegistrationId = it => it['id'];
-
   let storeState;
 
   beforeEach(() => {
-    const createRegistration = (id: string, name = ''): RegistrationEntity => ({
+    const createRegistration = (): RegistrationEntity => ({
       loading: false,
       registrationSucceeded: false
     });
+
     storeState = {
-      registration: {
-        list: [
-          createRegistration('PRODUCT-AAA'),
-          createRegistration('PRODUCT-BBB'),
-          createRegistration('PRODUCT-CCC')
-        ],
-        selectedId: 'PRODUCT-BBB',
+        entity: {},
         error: ERROR_MSG,
-        loaded: true
-      }
-    };
+      };
   });
 
   describe('Registration Selectors', () => {
-    it('getAllRegistration() should return the list of Registration', () => {
-      const result = registrationQuery.getRegistrationSuccessfull(storeState);
-      expect(result).toBe(false);
-    });
-
-    it('getSelectedRegistration() should return the selected Entity', () => {
-      const result = registrationQuery.getError(storeState);
-      expect(result).toBe('');
-    });
-
-    it("getLoading() should return the current 'loaded' status", () => {
+    it('getLoading() should return the loading status', () => {
       const result = registrationQuery.getLoading(storeState);
       expect(result).toBe(false);
     });
 
-    it("getError() should return the current 'error' storeState", () => {
+    it('getError() should return the error message', () => {
       const result = registrationQuery.getError(storeState);
-
       expect(result).toBe(ERROR_MSG);
+    });
+
+    it('getRegistrationSuccessfull() should return the current successfull registration status', () => {
+      const result = registrationQuery.getRegistrationSuccessfull(storeState);
+      expect(result).toBe(false);
     });
   });
 });
